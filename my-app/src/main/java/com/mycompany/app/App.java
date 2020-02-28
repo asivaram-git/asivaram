5edac46c3ffecc461aee9257e1d506fb1ddd36f1
asivaram-git <54541341+asivaram-git@users.noreply.github.com>
2020-02-28 14:58:54 +0530

    Updated App.java with 3 hello world

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "1.Hello World!" );
+        System.out.println( "2.Hello World!" );
+        System.out.println( "3.Hello World!" );
     }
 }
